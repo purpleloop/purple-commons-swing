1a811e6934b19eebdc9e97c516d66811b289343a
purpleloop <38167219+purpleloop@users.noreply.github.com>
2025-05-05 03:01:27 +0200

    * Export graph exceptions

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module io.github.purpleloop.commons.swing {
     exports io.github.purpleloop.commons.swing.graph;
     exports io.github.purpleloop.commons.swing.graph.shapes;
     exports io.github.purpleloop.commons.swing.graph.algorithm;
+    exports io.github.purpleloop.commons.swing.graph.exception;
     exports io.github.purpleloop.commons.swing.sprites;
     exports io.github.purpleloop.commons.swing.sprites.model;
     exports io.github.purpleloop.commons.swing.sprites.exception;
